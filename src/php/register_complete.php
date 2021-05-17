85dd6f74f5e39803f907a858c8311e1f89a18f03
Darak222 <tobi_sobota@onet.eu>
2021-05-17 09:42:35 +0200

    Registration - check if login or email already in database

--- src/php/register_complete.php
+++ src/php/register_complete.php
@@ -3,7 +3,19 @@
 <?php
 	$login = $_POST['username'];
 	$email = $_POST['email'];
-	$password = $_POST['password'];
-	mysqli_query($db, "INSERT INTO users (`username`, `email`, `password`) VALUES ('$login', '$email', '$password')"); 
+	$checkLoginExists = mysqli_query($db, "SELECT DISTINCT * FROM users WHERE username = '$login'");
+	$checkEmailExists = mysqli_query($db, "SELECT DISTINCT * FROM users WHERE username = '$email'");
+	if(mysqli_num_rows($checkLoginExists) == 1){
+		$_SESSION['login_exists'] = true;
+		header("Location: register_wip.php");
+	}
+	else if(mysqli_num_rows($checkEmailExists) == 1){
+		$_SESSION['email_exists'] = true;
+		header("Location: register_wip.php");
+	}
+	else{
+		$password = $_POST['password'];
+		mysqli_query($db, "INSERT INTO users (`username`, `email`, `password`) VALUES ('$login', '$email', '$password')");
+		header("Location: login_wip.php");
+	}
 ?>
-<?php header("Location: login_wip.php"); ?>
